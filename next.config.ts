b52d569dbb0176fe5b70eeb1ff7857bbdf96fa91
Anton <antonkzavcenco300@gmail.com>
2026-05-09 00:18:59 +0200

    fix: turbopack path

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   turbopack: {
-    root: path.resolve(__dirname),
-  },
+    root: './'
+  }
 };
 
 export default nextConfig;
